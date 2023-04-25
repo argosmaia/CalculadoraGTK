46cea8dc22bc147f17bdcca1028a8ab906248bc3
Argos Maia <33440266+argosmaia@users.noreply.github.com>
2023-04-25 11:06:28 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,66 @@
+#include <gtk/gtk.h>
+#include <regex.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * @brief declaração de funções
+ * on_button_clicked: é a função de callback do botão, serve para calcular a
+ * expressão.
+ * 
+ * on_window_destroy: é a função de callback da janela, serve para fechar a
+ * janela.
+ * 
+ * validate_expression: é uma função que valida a expressão, usa regex para
+ * validar se uma expressão tem caracteres inválidos. 
+*/
+
+static void on_button_clicked(GtkWidget *widget, gpointer data);
+static void on_window_destroy(GtkWidget *widget, gpointer data);
+static gboolean validate_expression(const char *expression);
+
+static void on_button_clicked(GtkWidget *widget, gpointer data) {
+    GtkWidget *entry = (GtkWidget *) data;
+    const char *expression = gtk_entry_get_text(GTK_ENTRY(entry));
+    if (validate_expression(expression)) {
+        double result = 0;
+        char *endptr; //endptr é um ponteiro para char que aponta para o primeiro caractere não convertido.
+        result = strtod(expression, &endptr);
+        //strtod: função da biblioteca padrão do C que converte uma string para
+        if (result == 0 && endptr == expression) {
+            gtk_entry_set_text(GTK_ENTRY(entry), "Erro");
+        } else {
+            gtk_entry_set_text(GTK_ENTRY(entry), endptr);
+        }
+    } else {
+        gtk_entry_set_text(GTK_ENTRY(entry), "Erro");
+    }
+}
+
+static void on_window_destroy(GtkWidget *widget, gpointer data) {
+    gtk_main_quit();
+}
+
+static gboolean validate_expression(const char *expression) {
+    regex_t regex;
+    int reti;
+    char msgbuf[100];
+
+    reti = regcomp(&regex, "^[0-9+\\-*/^()\\. ]+$", 0);
+    if (reti) {
+        fprintf(stderr, "Não consegui compilar a expressão gerada\n");
+        exit(1);
+    }
+
+    reti = regexec(&regex, expression, 0, NULL, 0);
+    if (!reti) {
+        return TRUE;
+    } else if (reti == REG_NOMATCH) {
+        return FALSE;
+    } else {
+        regerror(reti, &regex, msgbuf, sizeof (msgbuf));
+        fprintf(stderr, "combinação falhou: %s\n", msgbuf);
+        exit(1);
+    }
+}
